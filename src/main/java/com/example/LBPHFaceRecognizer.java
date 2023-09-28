a958fc6f14c13f56e306f22295916d192edf06f7
immkavin-ranks <immkavin.ofcl@gmail.com>
2023-09-28 15:01:54 +0530

    Attempt to implement face recognition

---
+++ src/main/java/com/example/LBPHFaceRecognizer.java
@@ -0,0 +1,33 @@
+package com.example;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.core.CvType;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.CvType;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.MatOfPoint3f;
+import org.opencv.core.MatOfDouble;
+import org.opencv.core.CvType;
+
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.videoio.VideoCapture;
+import org.opencv.videoio.VideoWriter;
+import org.opencv.videoio.*; // VideoWriter_fourcc;
+//import org.opencv.face.FaceRecognizer;
+//import org.opencv.face.LBPHFaceRecognizer;
+
+
+public class LBPHFaceRecognizer {
+   // FaceRecognizer recognizer = LBPHFaceRecognizer.create();
+   // recognizer.read("haarcascade_profileface.xml");
+}
